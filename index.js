91a843c78ac8f41daa4f9ac9d9cf03b0e407bf62
Raksyak-MST <rakshyak@ms-tech.in>
2025-08-29 19:15:52 +0530

    Add SQL queries and API endpoints for hotel templates

--- index.js
+++ index.js
@@ -10,16 +10,110 @@ const connection = mysql.createConnection({
   database: "Template",
 });
 
-app.get("/sections/:hotelID/:templateID", async (req, res) => {
-  console.log(req.originalUrl);
-  connection.query("SELECT * FROM contents", (err, result) => {
-    console.log(result);
-    res.send("done");
-  })
+app.get("/:hotelID", async (req, res) => {
+  try {
+    const hotel = await getHotel(req.params.hotelID);
+    res.status(200).send({ hotel });
+  } catch (error) {
+    res.status(403).send({ error: "Not able to process request", reason: error });
+  }
 });
 
-app.listen(8000, () =>
-  console.log(
-    `API service started on port 8000\n Connection to db : ${connection}`,
-  ),
-);
+app.get("/:template_page_id/sections", async (req, res) => {
+  const { template_page_id } = req.params;
+  const sections = await getSections(template_page_id);
+  res.status(200).send({
+    sections
+  })
+})
+
+app.get("/:template_section_id/contents", async(req, res) => {
+  const { template_section_id } = req.params;
+  const contents = await getContents(template_section_id); 
+  res.status(200).send({
+    contents
+  })
+})
+
+app.get("/:template_section_id/media-content", async(req, res) => {
+  const { template_section_id } = req.params;
+  const media_content = await getMediaContens(template_section_id);
+  res.status(200).send({
+    media_content
+  })
+})
+
+const getHotel = async (hotelID) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM hotels where id = ?",
+      hotelID,
+      (err, result) => {
+        if (err || !result) {
+          console.log(err);
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      },
+    );
+  });
+};
+
+const getPages = async (template_id) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM template_pages where template_id = ?",
+      template_id,
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      },
+    );
+  });
+};
+
+const getSections = async (templage_page_id) => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM template_sections where template_page_id = ?",
+      templage_page_id,
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      },
+    );
+  });
+};
+
+const getContents = async () => {
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT * FROM contents", (err, result) => {
+      if(err){
+        reject(err);
+      }else{
+        resolve(result);
+      }
+    })
+  })
+}
+
+const getMediaContens = async (template_section_id) => {
+return new Promise((resolve, reject) => {
+  connection.query("SELECT url, alt_text, media_type, order_index FROM media_content where template_section_id = ?", template_section_id, (err, result) => {
+    if(err){
+      reject(err);
+    }else {
+      resolve(result);
+    }
+  })
+})  
+}
+
+app.listen(8000, () => console.log("API service started on port 8000"));
